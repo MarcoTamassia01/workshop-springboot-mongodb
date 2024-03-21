c354a6d91f5d6251815914680b3c8aff45b37792
Marco Sallas <marco.a.s.tamassia@gmail.com>
2024-03-20 23:03:36 -0300

    Atualização de usuário com PUT

--- src/main/java/com/marco/workshopmongo/resources/UserRosource.java
+++ src/main/java/com/marco/workshopmongo/resources/UserRosource.java
@@ -10,9 +10,9 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -54,4 +54,14 @@ public class UserRosource {
 		return ResponseEntity.noContent().build();
  }
 	
+	
+	@PutMapping(value = "/{id}")
+ 	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User obj = service.fromDTO(objDto);
+		obj.setId(id); 
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	
+	
+	}
 }
